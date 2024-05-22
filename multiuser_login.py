884412626299b312bc0b9f3f2aff5bbd2612d750
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-22 18:43:08 +0200

    Update multiuser_login.py

--- multiuser_login.py
+++ multiuser_login.py
@@ -3,6 +3,6 @@ import pandas as pd
 def csv_reader(csv):
     return pd.read_csv(csv, sep= ";")
 
-Rezepte_dataframe = csv_reader("Rezepte_dataframe")
+Rezepte_dataframe = csv_reader("test")
 
 st.markdown(Rezepte_dataframe)
