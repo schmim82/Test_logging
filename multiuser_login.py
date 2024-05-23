8f0494aa1414ad11337adf4371107ef66459cff1
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-23 19:44:12 +0200

    Update multiuser_login.py

--- multiuser_login.py
+++ multiuser_login.py
@@ -10,10 +10,14 @@ if 'selected_option' not in st.session_state:
 # Funktion zum Aktualisieren der Selectbox
 def set_selectbox_to_three():
     st.session_state.selected_option = '3'
+    st.experimental_rerun()
 
 # Erstelle die Selectbox in der Sidebar mit dem initialen Wert aus st.session_state
-selected = st.sidebar.selectbox("Wähle eine Option", options, key='selectbox', index=options.index(st.session_state.selected_option))
+selected = st.sidebar.selectbox("Wähle eine Option", options, index=options.index(st.session_state.selected_option), key='selectbox')
 
 # Button, der beim Drücken die Selectbox auf '3' setzt
 if st.sidebar.button("Setze auf 3"):
     set_selectbox_to_three()
+
+# Zeige die aktuelle Auswahl an
+st.write(f"Ausgewählte Option: {selected}")
