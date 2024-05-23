96645af0c1b15e7a1295af2cfccd9caed384f756
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-23 19:56:33 +0200

    Update multiuser_login.py

--- multiuser_login.py
+++ multiuser_login.py
@@ -1,20 +1,21 @@
-import streamlit as st
-
 st.set_page_config(page_title="test")
 st.title("test")
 
 st.sidebar.title("test")
 
+# Initialisiere den Zustand, falls er noch nicht existiert
+if 'selected_option' not in st.session_state:
+    st.session_state.selected_option = '1'  # Setze die Standardauswahl auf '1'
 
+# Button, der den Zustand ändert
+if st.sidebar.button("drücken"):
+    st.session_state.selected_option = '2'
 
+# Optionen für das Radio-Button
+liste = ["1", "2"]
 
-button = st.sidebar.button("drücken")
+# Erstelle das Radio-Button mit der ausgewählten Option aus st.session_state
+selected = st.sidebar.radio("test", liste, index=liste.index(st.session_state.selected_option))
 
-
-if button:
-  liste = ["2"]
-
-else:
-  liste = ["1", "2"]
-  
-st.sidebar.radio("test", liste)
+# Zeige die aktuelle Auswahl an
+st.write(f"Ausgewählte Option: {selected}")
