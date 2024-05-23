53ef40dcc2459774cd499c907d573b60e66b078b
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-23 19:52:28 +0200

    Update multiuser_login.py

--- multiuser_login.py
+++ multiuser_login.py
@@ -1,23 +1,10 @@
 import streamlit as st
 
-# Definiere die Optionen für die Selectbox
-options = ['1', '2', '3']
+st.set_page_config(page_title="test")
+st.title("test")
 
-# Initialisiere den Zustand der Selectbox in st.session_state
-if 'selected_option' not in st.session_state:
-    st.session_state.selected_option = '1'
+st.sidebar.title("test")
 
-# Funktion zum Aktualisieren der Selectbox
-def set_selectbox_to_three():
-    st.session_state.selected_option = '3'
-    st.rerun()
+liste = ["1", "2"]
 
-# Erstelle die Selectbox in der Sidebar mit dem initialen Wert aus st.session_state
-selected = st.sidebar.selectbox("Wähle eine Option", options, index=options.index(st.session_state.selected_option), key='selectbox')
-
-# Button, der beim Drücken die Selectbox auf '3' setzt
-if st.sidebar.button("Setze auf 3"):
-    set_selectbox_to_three()
-
-# Zeige die aktuelle Auswahl an
-st.write(f"Ausgewählte Option: {selected}")
+st.sidebar.radio("test", liste)
