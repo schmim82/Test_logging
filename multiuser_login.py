cd01319a47839271ac06a81574a51ec2a1592f54
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-25 15:42:22 +0200

    Update multiuser_login.py

--- multiuser_login.py
+++ multiuser_login.py
@@ -3,8 +3,7 @@ import streamlit as st
 import pandas as pd
 from github_contents import GithubContents
 import Zutaten_daten as zd
-from test_2 import bilder
-from bilder import pinkeblume
+import os
 
 
 
