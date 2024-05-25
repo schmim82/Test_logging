38989601119fb49e0f219f8a99c435d7cf0b0d99
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-25 15:30:14 +0200

    Update multiuser_login.py

--- multiuser_login.py
+++ multiuser_login.py
@@ -1,63 +1,10 @@
 
-
-
-
-
 import streamlit as st
 import pandas as pd
 from github_contents import GithubContents
 import Zutaten_daten as zd
 
-def einkaufsliste_erstellen(einkaufsliste, Kochbuch):
-    leere_dic = {}
-    leere_dic_2 = {}
 
-    for key in einkaufsliste:
-        dictionary = Kochbuch[key]
-        anzahl = einkaufsliste[key]
-
-        for key in dictionary:
-            words = key.split()
-            if words[0] != "Zubereitung":
-                if key not in leere_dic:
-                    if isinstance(dictionary[key], int) or isinstance(dictionary[key], float):
-                        leere_dic[key] = dictionary[key] * anzahl
-                    else:
-                        leere_dic[key] = dictionary[key]
-                else:
-                    if isinstance(dictionary[key], int) or isinstance(dictionary[key], float):
-                        leere_dic[key] += dictionary[key] * anzahl
-
-    for key in leere_dic:
-        if isinstance(leere_dic[key], int) or isinstance(leere_dic[key], float):
-            leere_dic_2[key] = leere_dic[key]
-        else:
-            words_2 = key.split()
-            if words_2[0] not in leere_dic_2:
-                leere_dic_2[words_2[0]] = ""
-
-    for key in leere_dic_2:
-        words_3 = key.split()
-        if leere_dic_2[key] != "":
-            if len(words_3) == 1:
-                # Markdown-Text mit Checkbox anzeigen
-                checkbox_key = st.checkbox(f"{key} -- {leere_dic_2[key]} gramm")
-            else:
-                checkbox_key = st.checkbox(f"{key} -- {leere_dic_2[key]}")
-        else:
-            checkbox_key = st.checkbox(f"{key}")
-
-
-
-
-
-liste = {"Goma Ae": 2,
-        "Knoblauchbrot": 2,
-        "Parmigiana": 2,
-        "Gefüllte Tomaten mit Feta": 2,
-        "Kichererbsenbällchen mit Aprikosen": 2}
-
-einkaufsliste_erstellen(liste, zd.Kochbuch)
 
 
 
