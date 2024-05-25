c669e57c3348236fd82ec4ed2686402c971245d9
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-25 14:01:15 +0200

    Update multiuser_login.py

--- multiuser_login.py
+++ multiuser_login.py
@@ -1,3 +1,8 @@
+
+
+
+
+
 import streamlit as st
 import pandas as pd
 from github_contents import GithubContents
@@ -7,62 +12,46 @@ def einkaufsliste_erstellen(einkaufsliste, Kochbuch):
     leere_dic = {}
     leere_dic_2 = {}
 
-    
-
     for key in einkaufsliste:
         dictionary = Kochbuch[key]
-
         anzahl = einkaufsliste[key]
 
         for key in dictionary:
-
             words = key.split()
-        
-
             if words[0] != "Zubereitung":
-
-                
-
                 if key not in leere_dic:
-
                     if isinstance(dictionary[key], int) or isinstance(dictionary[key], float):
                         leere_dic[key] = dictionary[key] * anzahl
-
                     else:
                         leere_dic[key] = dictionary[key]
-
                 else:
-
                     if isinstance(dictionary[key], int) or isinstance(dictionary[key], float):
-                        leere_dic[key] = leere_dic[key] + (dictionary[key] * anzahl)
-
-
+                        leere_dic[key] += dictionary[key] * anzahl
 
     for key in leere_dic:
         if isinstance(leere_dic[key], int) or isinstance(leere_dic[key], float):
             leere_dic_2[key] = leere_dic[key]
-
         else:
             words_2 = key.split()
-            if words_2[0]  not in leere_dic_2:
-
+            if words_2[0] not in leere_dic_2:
                 leere_dic_2[words_2[0]] = ""
 
-
-
-                
     for key in leere_dic_2:
         words_3 = key.split()
         if leere_dic_2[key] != "":
             if len(words_3) == 1:
-                st.markdown(f"{key} -- {leere_dic_2[key]} gramm")
-
+                # Markdown-Text und Checkbox anzeigen
+                checked = st.checkbox(f"{key} -- {leere_dic_2[key]} gramm")
+                if checked:
+                    st.markdown(f"~~{key} -- {leere_dic_2[key]} gramm~~")
             else:
-                st.markdown(f"{key} -- {leere_dic_2[key]}")
-
+                checked = st.checkbox(f"{key} -- {leere_dic_2[key]}")
+                if checked:
+                    st.markdown(f"~~{key} -- {leere_dic_2[key]}~~")
         else:
-            st.markdown(f"{key}")
-
+            checked = st.checkbox(f"{key}")
+            if checked:
+                st.markdown(f"~~{key}~~")
 
 
 
