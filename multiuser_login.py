992bf60f27bc492de34880657b47a5cf24ef9f89
schmim82 <162346351+schmim82@users.noreply.github.com>
2024-05-25 11:19:35 +0200

    Update multiuser_login.py

--- multiuser_login.py
+++ multiuser_login.py
@@ -1,23 +1,42 @@
 import streamlit as st
+import pandas as pd
+from github_contents import GithubContents
 
 st.set_page_config(page_title="test")
-st.title("test")
 
-st.sidebar.title("test")
+# Set constants
+DATA_FILE = "test.csv"
+DATA_COLUMNS = ['Name', 'Rezept', 'Anzahl']
 
-# Initialisiere den Zustand, falls er noch nicht existiert
-if 'selected_option' not in st.session_state:
-    st.session_state.selected_option = '1'  # Setze die Standardauswahl auf '1'
+def init_github():
+    """Initialisiere das GithubContents-Objekt."""
+    if 'github' not in st.session_state:
+        st.session_state.github = GithubContents(
+            st.secrets["github"]["owner"],
+            st.secrets["github"]["repo"],
+            st.secrets["github"]["token"])
+        print("GitHub initialisiert")
 
-# Button, der den Zustand ändert
-if st.sidebar.button("drücken"):
-    st.session_state.selected_option = '2'
+def init_credentials():
+    """Initialisiere oder lade das DataFrame."""
+    if 'df_users' not in st.session_state:
+        if st.session_state.github.file_exists(DATA_FILE):
+            st.session_state.df_users = st.session_state.github.read_df(DATA_FILE)
+        else:
+            st.session_state.df_users = pd.DataFrame(columns=DATA_COLUMNS)
 
-# Optionen für das Radio-Button
-liste = ["1", "2"]
+def save_to_csv(dataframe):
+    """Speichere das DataFrame in einer CSV-Datei."""
+    st.session_state.github.write_df(DATA_FILE, dataframe, "updated CSV")
 
-# Erstelle das Radio-Button mit der ausgewählten Option aus st.session_state
-selected = st.sidebar.radio("test", liste, index=liste.index(st.session_state.selected_option))
+init_github() # Initialisiere das GithubContents-Objekt
+init_credentials() # Lade die Anmeldeinformationen aus dem GitHub-Datenrepository
+save_to_csv(st.session_state.df_users) # Speichere das DataFrame in der CSV-Datei
+new_data = {'Name': ['Neue Zutat'], 'Rezept': ['Neues Rezept'], 'Anzahl': [10]}
+new_data_df = pd.DataFrame(new_data)
 
-# Zeige die aktuelle Auswahl an
-st.write(f"Ausgewählte Option: {selected}")
+# DataFrame aktualisieren
+st.session_state.df_users = pd.concat([st.session_state.df_users, new_data_df], ignore_index=True)
+
+# DataFrame in CSV-Datei speichern
+save_to_csv(st.session_state.df_users)
